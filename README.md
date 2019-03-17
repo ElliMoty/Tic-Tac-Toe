d279f6c0b81b880cabfd99d9d242b7c172e293e8
Elli <ellimoty@gmail.com>
2019-03-17 21:21:31 +1100

    Deployed in Heroku and chnaged some links and README

--- README.md
+++ README.md
@@ -3,16 +3,9 @@
 # Tic Tac Toe 
 > It seems to be a simple game at first glimpse but it will turn to a challenging one soon if you have a keen rival! :blush:
 
-## Installing
-
-Before starting the game you need to take following step.
-
-- **Clone this repository to your computer**
-    + *'Cloning' takes this repository on GitHub and creates a local copy on your computer.*
-    + Hit the **Clone or Download** button in the top right-hand corner of the page and copy the URL to your clipboard.
-
 ## Start the game 
-Open the html file in your browser.
+Before start the game please read the below instructions.
+[Demo](https://tic-tac-toe-project0.herokuapp.com/)
 
 ### Instructions
 - The first page gives you some visual information about how the game looks like.
@@ -21,13 +14,13 @@ Open the html file in your browser.
 
 - In the next page:
     + Enter your names.
-    + At the moment the markers will be selected randomly for you but :soon: you will have more options to select yourself.
+    + At this moment, the markers will be selected randomly for you but :soon: you will have more options to select yourself.
     + :soon: In near future another option to select the size of the board will be added.
     + Hit **GO** button to start the game.
 
 - here you are! :thumbsup:
-    + At the top of the page you can see and track your points you earn during the game in front of your names.
-    + Hit **NEXT** to try the game again wether you win or lose.
+    + At the top of the page you can see and track the points you earn during the game in front of your names.
+    + Hit **NEXT** to try the game again whether you win or lose.
     + Click **FINISH** button to finish the game and back to first page.
 
 **Note** Enjoy! Feedback welcome. :blush:
--- js/ui.js
+++ js/ui.js
@@ -2,172 +2,186 @@
 const sources = game.src();
 
 function gameInfo(n, element) {
-    let place1;
-    let place2;
-    if (n % 2 === 1) {
-        src = sources.path1;
-        color = '#def1ff';
-        place1 = element.attr('id');
+  let place1;
+  let place2;
+  if (n % 2 === 1) {
+    src = sources.path1;
+    color = "#def1ff";
+    place1 = element.attr("id");
+  } else if (n % 2 === 0) {
+    src = sources.path2;
+    color = "#e8c0bd";
+    place2 = element.attr("id");
+  }
 
-    } else if (n % 2 === 0) {
-        src = sources.path2;
-        color = '#e8c0bd';
-        place2 = element.attr('id');
-    }
-
-    return { src, color, place1, place2 };
+  return { src, color, place1, place2 };
 }
 
 $(document).ready(() => {
 
-    // GO button
-    $('#go').on('click', () => {
+  // GO button
+  $("#go").on("click", () => {
+    const name1 = $("#user1-name").val();
+    // save info in localStorage
+    localStorage.name1 = name1;
+    const name2 = $("#user2-name").val();
+    localStorage.name2 = name2;
+  });
 
-        // $("a").css('href', '#game');
-        const name1 = $('#user1-name').val();
-        const name2 = $('#user2-name').val();
+  // play game
 
-        if (name1 && name2) {
+  let click = 1;
+  let point1 = 0;
+  let point2 = 0;
+  let position1 = [];
+  let position2 = [];
+  let winner1;
+  let winner2;
 
-            $('#name1').attr('value', name1);
-            $('#name2').attr('value', name2);
+  // Read localStorage
+  const name1 = localStorage.name1;
+  const name2 = localStorage.name2;
 
-            $('#p1').val(point1);
-            $('#p2').val(point2);
+  if (name1 && name2) {
+    $("#name1").attr("value", name1);
+    $("#name2").attr("value", name2);
 
-            $('<img id="marker1" class="sign inline">').appendTo('.info1');
-            $('#marker1').attr('src', sources.path1);
+    $("#p1").val(point1);
+    $("#p2").val(point2);
 
-            $('<img id="marker2" class="sign inline">').appendTo('.info2')
-            $('#marker2').attr('src', sources.path2);
+    $('<img id="marker1" class="sign inline">').appendTo(".info1");
+    $("#marker1").attr("src", sources.path1);
+
+    $('<img id="marker2" class="sign inline">').appendTo(".info2");
+    $("#marker2").attr("src", sources.path2);
+  }
+
+  $(".cell").on("click", function() {
+    let $this = $(this);
+    let $thisId = $this.attr("id");
+
+    if (position1.indexOf($thisId) >= 0) {
+      return;
+    }
+
+    if (position2.indexOf($thisId) >= 0) {
+      return;
+    }
+
+    if (winner1 || winner2) {
+      return;
+    }
+
+    const result = gameInfo(click, $this);
+
+    $this.css("background-color", result.color);
+    $("<img>")
+      .attr("src", result.src)
+      .appendTo($this);
+
+    if (result.place1) {
+      position1.push(result.place1);
+    }
+    if (result.place2) {
+      position2.push(result.place2);
+    }
+
+    click++;
+
+    if (click >= 5) {
+        // looping over options
+      for (let i = 0; i < game.options.length; i++) {
+        winner1 = true;
+        winner2 = true;
+        const option = game.options[i];
+        // looping over each array inside options
+        for (let j = 0; j < option.length; j++) {
+          if (position1.indexOf(option[j]) < 0) {
+            winner1 = false;
+          }
+          if (position2.indexOf(option[j]) < 0) {
+            winner2 = false;
+          }
+          if (!winner1 && !winner2) {
+            break;
+          }
         }
-    });
-
-    // play game
-
-    let click = 1;
-    let point1 = 0;
-    let point2 = 0;
-    let position1 = [];
-    let position2 = [];
-    let winner1;
-    let winner2;
-
-    $('.cell').on('click', function () {
-        let $this = $(this);
-        let $thisId = $this.attr('id');
-
-        if (position1.indexOf($thisId) >= 0) {
-            return;
-        }
-
-        if (position2.indexOf($thisId) >= 0) {
-            return;
-        }
-
         if (winner1 || winner2) {
-            return;
+          break;
         }
+      }
 
-        const result = gameInfo(click, $this);
+      // winner part
+      if (winner1) {
+        point1++;
+        $("#p1").val(point1);
+        $("#game-board").css("display", "none");
+        $("#winner")
+          .css("display", "block")
+          .fadeIn(4000);
+      }
 
-        $this.css('background-color', result.color);
-        $('<img>').attr('src', result.src).appendTo($this);
+      if (winner2) {
+        point2++;
+        $("#p2").val(point2);
+        $("#game-board").css("display", "none");
+        $("#winner")
+          .css("display", "block")
+          .fadeIn(4000);
+      }
+    }
 
-        if (result.place1) {
-            position1.push(result.place1);
-        }
-        if (result.place2) {
-            position2.push(result.place2);
-        }
+    if (click === 9) {
+      if (!winner1 && !winner2) {
+        $("#game-board").css("display", "none");
+        $("#loser")
+          .css("display", "block")
+          .fadeIn(4000);
+      }
+    }
+  });
 
-        click++;
+  // next button
+  $("#restart").on("click", () => {
+    if ($("#winner").css("display", "block")) {
+      $("#winner")
+        .css("display", "none")
+        .fadeOut(4000);
+    }
 
-        if (click >= 5) {
+    if ($("#loser").css("display", "block")) {
+      $("#loser")
+        .css("display", "none")
+        .fadeOut(4000);
+    }
 
-            for (let i = 0; i < game.options.length; i++) {
-                winner1 = true;
-                winner2 = true;
-                const option = game.options[i];
-                for (let j = 0; j < option.length; j++) {
-                    if (position1.indexOf(option[j]) < 0) {
-                        winner1 = false;
-                    }
-                    if (position2.indexOf(option[j]) < 0) {
-                        winner2 = false;
-                    }
-                    if (!winner1 && !winner2) {
-                        break;
-                    }
-                }
-                if (winner1 || winner2) {
-                    break;
-                }
-            }
+    $("#game-board").css("display", "block");
+    $(".cell").css("background-color", "#252d65");
+    $(".cell img").remove();
 
-            // winner part
-            if (winner1) {
-                point1++;
-                $('#p1').val(point1);
-                $('#game-board').css('display', 'none');
-                $('#winner').css('display', 'block').fadeIn(4000);
-            }
+    click = 1;
+    position1 = [];
+    position2 = [];
+    winner1 = false;
+    winner2 = false;
+  });
 
-            if (winner2) {
-                point2++;
-                $('#p2').val(point2);
-                $('#game-board').css('display', 'none');
-                $('#winner').css('display', 'block').fadeIn(4000);
-            }
-        }
+  // finish button
+  $("#finish").on("click", () => {
+    // $('a').attr('href', '#home');
 
-        if (click === 9) {
-            if (!winner1 && !winner2) {
-                $('#game-board').css('display', 'none');
-                $('#loser').css('display', 'block').fadeIn(4000);
-            }
-        }
-    });
+    $("#user1-name").val("");
+    $("#user1-name").attr("placeholder", "Player#1 Name:");
+    $("#user2-name").val("");
+    $("#user2-name").attr("placeholder", "Player#2 Name:");
 
-    // next button
-    $('#restart').on('click', () => {
+    $("#name1").removeAttr("value");
+    $("#name2").removeAttr("value");
 
-        if ($('#winner').css('display', 'block')) {
-            $('#winner').css('display', 'none').fadeOut(4000);
-        }
+    $("#p1").val("");
+    $("#p2").val("");
 
-        if ($('#loser').css('display', 'block')) {
-            $('#loser').css('display', 'none').fadeOut(4000);
-        }
-
-        $('#game-board').css('display', 'block');
-        $('.cell').css('background-color', '#252d65');
-        $('.cell img').remove();
-
-        click = 1;
-        position1 = [];
-        position2 = [];
-        winner1 = false;
-        winner2 = false;
-    });
-
-    // finish button 
-    $('#finish').on('click', () => {
-        // $('a').attr('href', '#home');
-
-        $('#user1-name').val('');
-        $('#user1-name').attr('placeholder', 'Player#1 Name:');
-        $('#user2-name').val('');
-        $('#user2-name').attr('placeholder', 'Player#2 Name:');
-
-        $('#name1').removeAttr('value');
-        $('#name2').removeAttr('value');
-
-        $('#p1').val('');
-        $('#p2').val('');
-
-        $('.info1 img').remove();
-        $('.info2 img').remove();
-
-    });
+    $(".info1 img").remove();
+    $(".info2 img").remove();
+  });
 });
